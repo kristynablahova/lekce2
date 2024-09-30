3b428c03b09e997e83627200a492fec813f03f94
Kristýna Bláhová <kristynablahova1234@gmail.com>
2024-09-30 20:11:22 +0200

    uloha1

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ const pocetPredstaveni = 15
 
 const prijem = 12 * 174 * 15
 
-document.body.innerHTML += prijem 
+document.body.innerHTML += "<p>" + prijem + "</p>"
 
+const vstupneStudent = cena * 0.65
+const studenti = pocetNavstevniku * 0.4
+const pocetDospely = 174 - studenti
+
+const prijem2 = ((pocetDospely * 12) + (studenti * vstupneStudent)) * 15
+
+document.body.innerHTML += "<p>" + "celkový příjem divadla je:"+ " " + prijem2 + " " + "EUR" + "</p>"
